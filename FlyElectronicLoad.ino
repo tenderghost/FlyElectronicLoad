510bc2de2849e18c318a0a3e46ed251530b529ae
tenderghost <tenderghost@gmail.com>
2017-10-17 23:11:59 +0800

    Add push button support

--- FlyElectronicLoad.ino
+++ FlyElectronicLoad.ino
@@ -1,6 +1,7 @@
 /**
  * FlyElectronicLoad by tenderghost@gmail.com
  * Using arduino pro mini
+ * Using https://www.arduino.cc/en/Tutorial/Debounce for pushbutton to ignore noise
  */
 
 // Pin definations
@@ -18,9 +19,25 @@ int dischagePWMSpeed = 0;
 float batVoltage = 0.0;
 float batDischageCurrent = 0.0;
 
+// Debounce begin
+// Variables will change:
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+// Debounce end
+
+// 记录采样时间，间隔1秒采样，避免采样频率过高，影响按钮控制
+unsigned long lastSampleTime = 0;
+unsigned long sampleDelay = 1000; // in milliseconds
+
 const float LiChargedVtg = 4.2;
 const float LiCutoffVtg = 3.0;
-const unsigned int VOLT_SAMPLE_COUNT = 1000; // Voltage or Current sample times per loop
+
+const unsigned int VOLT_SAMPLE_COUNT = 100; // Voltage or Current sample times per loop
 
 // Electronic load running state constants
 const unsigned int STATE_WATING = 81;    // Waiting for command
@@ -29,6 +46,7 @@ unsigned int currentState = STATE_WATING;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(PUSH_BTN_PIN, INPUT);
   pinMode(MOS_PWM_PIN, OUTPUT);
   //pinMode(BAT_VOLTAGE_PIN, INPUT);
   //pinMode(CUR_VOLTAGE_PIN, INPUT);
@@ -37,28 +55,79 @@ void setup() {
 }
 
 void loop() {
-  int val = digitalRead(PUSH_BTN_PIN);  // read input value
-  if (val == LOW) {         // check if the input is LOW (button pushed)
-    Serial.println("Push button pushed.");
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(PUSH_BTN_PIN);
+  
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // button state change to HOW means the button was pushed.
+      if (buttonState == LOW) {
+        Serial.println("Push button pushed.");
+
+        // Change current Arduino state
+        if (currentState == STATE_WATING) {
+          currentState = STATE_RUNNING;
+        } else {  // discharging
+          currentState = STATE_WATING;
+        }
+
+      }
+    }
+  }
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+
+  // ==========================================
+  // DISCHAGE OR STOP DISCHAGE CODE START HERE!
+  if (currentState == STATE_RUNNING) {
+    if ((millis() - lastSampleTime) > sampleDelay) {
+      lastSampleTime = millis();
+      Serial.println("DO DISCHAGE");
+
+      // Read battery voltage and dischage current, values store to batVoltage and batDischageCurrent variables.
+      readVoltAndCurrent();
+
+      // test code
+      // set dischage current to 0.5A
+      if (batDischageCurrent < 0.5) {
+        Serial.println("Test code for change current under 0.5A");
+        int newPMWSpeed = dischagePWMSpeed + 1;
+        setPMWSpeed(newPMWSpeed);
+      }
+
+      Serial.print("Battery voltage:");
+      Serial.println(batVoltage);
+      Serial.println("===============");
+    }
+
+
+  } else {
+    if ((millis() - lastSampleTime) > sampleDelay) {
+      lastSampleTime = millis();
+
+      Serial.println("GO WAIT");
+      setPMWSpeed(0);
+    }
   }
 }
 
-void loop_old() {
-  // Read battery voltage and dischage current, read them from batVoltage and batDischageCurrent variables.
-  readVoltAndCurrent();
-
-  // test code
-  // set dischage current to 0.5A
-  if (batDischageCurrent < 0.5) {
-    Serial.println("Test code for change current under 0.5A");
-    int newPMWSpeed = dischagePWMSpeed + 1;
-    setPMWSpeed(newPMWSpeed);
-  }
-
-  Serial.println("===============");
-
-  delay(100);
-}
 
 /**
    Read voltage and current from battery.
@@ -74,8 +143,8 @@ void readVoltAndCurrent() {
     float fltVoltPinValue = totalVoltage / VOLT_SAMPLE_COUNT;
     float batVoltage = 2.0 * 5.0 * (fltVoltPinValue / 1023.0f);
   
-    Serial.print("Battery voltage:");
-    Serial.println(batVoltage);
+//    Serial.print("Battery voltage:");
+//    Serial.println(batVoltage);
   
     // get sampling resistor voltage drop to calculate current.
     totalVoltage = 0;
@@ -85,12 +154,12 @@ void readVoltAndCurrent() {
   
     float devidedVoltageDrop = 1.0 * totalVoltage / VOLT_SAMPLE_COUNT;
     //float voltageDrop = 5.0 * samplerResVoltDrop / 1023.0;
-    Serial.print("Voltage drop:");
-    Serial.println(devidedVoltageDrop);
+//    Serial.print("Voltage drop:");
+//    Serial.println(devidedVoltageDrop);
   
     batDischageCurrent = (5.0 * devidedVoltageDrop) / (1023.0 * rSensor);
-    Serial.print("Dischage Current:");
-    Serial.println(batDischageCurrent);
+//    Serial.print("Dischage Current:");
+//    Serial.println(batDischageCurrent);
 }
 
 void setPMWSpeed(int speed) {
